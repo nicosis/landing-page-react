a87567c2903633ea63def854092ca9cb07eb7855
nicosis <nicosis@gmail.com>
2023-03-29 19:28:24 +0000

    working

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
@@ -9,4 +10,4 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render( <Home />, document.querySelector("#app"));
